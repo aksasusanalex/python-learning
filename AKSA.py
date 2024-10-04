649cb7a5b2f5b446be7fb49419425faad00c4646
aksasusanalex <aksasusanalex2028@cs.sjcetpalai.ac.in>
2024-10-04 16:05:42 +0530

    Add files via upload

---
+++ AKSA.py
@@ -0,0 +1,39 @@
+#print("hello world")
+
+#print("hello\n world")
+'''
+if 5>2:
+              print("5 greater than two")
+if 5>2:
+    print("five is greater than two")
+
+x=54
+x="hffg"
+print(x,x)
+
+x=int(3)
+y=int(3)
+z=float(3)
+print(x)
+print(y)
+print(z)
+x=1j
+y="john"
+print(type(x))
+print(type(y))
+
+x,y,z="orange","banana","cherry"
+print(x)
+print(y)
+print(z)
+
+x="python"
+y=" is"
+z=" awesome"
+print(x + y + z)
+
+x=input("enter your name")
+print(x)
+'''
+a="hello world"
+print(a[5:7])
