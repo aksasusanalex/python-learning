2554209de49db5adeeee21e7688bded965a68cc0
aksasusanalex <aksasusanalex2028@cs.sjcetpalai.ac.in>
2024-10-25 14:30:35 +0530

    Add files via upload

---
+++ lab 3b.py
@@ -0,0 +1,9 @@
+
+'''
+check whether a number is positive or not
+'''
+x=int(input("enter the number"))
+if x>0:
+    print("it is positive")
+else:
+    print("it is negative")
---
+++ lab3.py
@@ -0,0 +1,18 @@
+
+
+n1=int(input("enter the first number"))
+n2=int(input("enter the second number"))
+n3=int(input("enter the third number"))
+l=0
+
+if n1>n2 and n1>n3:
+    l=n1
+
+
+elif n2>n3 :
+
+    l=n2
+else:
+    l=n3
+print("the largest number is",l)
+
